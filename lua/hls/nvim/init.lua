b6a08b54e70fc6e5a9c4228616ff4cc3dba1d1e3
gelegele <ryo500209@gmail.com>
2023-07-21 16:04:12 +0900

    add setup function

--- lua/hls/nvim/init.lua
+++ lua/hls/nvim/init.lua
@@ -1,11 +1,21 @@
 local M = {}
 
+-- Toggle highlight search
 function M.toggle()
   if vim.v.hlsearch == 1 then
     vim.cmd('nohl')
   else
     vim.api.nvim_feedkeys("*", "m", true)
   end
+end
+
+function M.setup()
+  local keymapoption = {
+    desc = 'hls.toggle',
+    noremap = true,
+    silent = true,
+  }
+  vim.keymap.set('n', '<C-n>', ':lua require("hls.nvim").toggle()<CR>', keymapoption)
 end
 
 return M
